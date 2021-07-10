502aa1b994f5d9eef59dd04b167496ae49b0d58d
ybgandhi <gandhibyash@gmail.com>
2021-07-10 18:00:03 -0400

    updated scrape_mars.py

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,18 +1,22 @@
 # import all dependencies
-from splinter import Browser
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 import requests
-import os
-import pandas as pd
 import pymongo
+from splinter import Browser
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import pandas as pd
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
+
 
 def chrome_browser():
     # set up splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    return Browser('chrome', **executable_path, headless=False)
+    return Browser('chrome', **executable_path)
+    #return Browser('chrome', **executable_path, headless=False)
 
 def scrape():
     browser = chrome_browser()
@@ -44,9 +48,33 @@ def scrape():
     jpl_url = "https://spaceimages-mars.com/"
     browser.visit(jpl_url)
 
+    #beautifil soup
     html = browser.html
     soup = bs(html, 'html.parser')
 
+    # find image
     image_url = soup.find('img', class_='thumbimg')['src']
     print(image_url)
+
+    featured_image_url = f"{jpl_url}{image_url}"
+
+    #************************************************
+    ## Mars Facts
+    #************************************************
+    mars_facts_url = "https://galaxyfacts-mars.com/"
+
+    tables = pd.read_html(mars_facts_url)
+
+    mars_fact=tables[0]
+    mars_fact=mars_fact.rename(columns={0:"Profile",1:"Value"},errors="raise")
+    mars_fact.set_index("Profile",inplace=True)
+    mars_fact
+
+    fact_table_html=mars_fact.to_html()
+    fact_table_html
+
+    fact_table_html.replace('\n','')
+    #print(fact_table_html)
+
+
 
